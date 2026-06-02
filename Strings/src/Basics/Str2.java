13303c3f6f9b9723e44bab08998c94a926fa4b10
Anshu Vishwakarma <145771009+vishwakarmaanshu@users.noreply.github.com>
2026-06-02 23:26:55 +0530

    Add files via upload

---
+++ Strings/src/Basics/Str2.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class Str2 {
+public static void main(String[] args) {
+		
+		
+		String s1 = "Bharat" ;
+		
+		String s2 = "India" ;
+		
+		String s3 = "Bharat" ;
+		
+		System.out.println(s1==s3);
+		System.out.println(s1.equals(s3));
+	}
+}
