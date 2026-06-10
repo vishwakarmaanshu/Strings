6d30d62f4d122d7360cced4bbf85f6d3c53c9213
Anshu Vishwakarma <145771009+vishwakarmaanshu@users.noreply.github.com>
2026-06-10 23:09:46 +0530

    Add files via upload

---
+++ Strings/src/Basics/Str4.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class Str4 {
+public static void main(String[] args) {
+		
+		String s = "Hello World" ;
+		
+		for (int i = 0; i < s.length(); i++) {
+			
+			System.out.println(s.charAt(i));
+		}
+	}
+}
